a8588ab44ec3dd8892b387844d3aac7bcb36ed5f
Izzat Fauzan <mizzatfauzan11@gmail.com>
2026-03-02 19:20:22 +0700

    feat: add encapsulation to Titik.java

--- Praktikum2/src/Titik.java
+++ Praktikum2/src/Titik.java
@@ -6,68 +6,68 @@
 
 public class Titik {
     /********************ATRIBUT*********************/
-    double absis;
-    double ordinat;
-    static int counterTitik = 0;
+    private double absis;
+    private double ordinat;
+    private static int counterTitik = 0;
 
     /********************METHOD**********************/
     //konstruktor untuk membuat titik (0,0)
-    Titik() {
+    public Titik() {
         absis = 0;
         ordinat = 0;
         counterTitik++;
     }
 
     //konstruktor untuk membuat titik dengan parameter absis dan ordinat tertentu
-    Titik(double absis, double ordinat){
+    public Titik(double absis, double ordinat){
         this.absis = absis;
         this.ordinat = ordinat;
         counterTitik++;
     }
 
     //mengembalikan nilai counterTitik
-    static int getCounterTitik(){
+    public static int getCounterTitik(){
         return counterTitik;
     }
 
     //mengembalikan nilai absis
-    double getAbsis() {
+    public double getAbsis() {
         return absis;
     }
 
     //mengembalikan nilai ordinat
-    double getOrdinat() {
+    public double getOrdinat() {
         return ordinat;
     }
 
     //mengeset absis titik dengan nilai baru x
-    void setAbsis(double x) {
+    public void setAbsis(double x) {
         absis = x;
     }
 
     //mengeset ordinat titik dengan nilai baru y
-    void setOrdinat(double y) {
+    public void setOrdinat(double y) {
         ordinat = y;
     }
 
     //menggeser nilai absis dan ordinat titik masing-masing sejauh x dan y
-    void geser(double x, double y) {
+    public void geser(double x, double y) {
         absis = absis + x;
         ordinat = ordinat + x;
     }
 
     //mencetak koordinat titik
-    void printTitik() {
+    public void printTitik() {
         System.out.println("Titik (" + absis + "," + ordinat + ")");
     }
 
     //mencetak getCounterTitik
-    void printCounterTitik(){
+    public void printCounterTitik(){
         System.out.println(this.counterTitik);
     }
 
     //mencari kuadran pada titik
-    int getKuadran(){
+    public int getKuadran(){
         if (absis > 0 && ordinat > 0){
             return 1;
         } else if (absis < 0 && ordinat > 0){
@@ -82,32 +82,32 @@ public class Titik {
     }
 
     //mencari jarakPusat
-    double getjarakPusat() {
+    public double getjarakPusat() {
         return Math.sqrt(absis * absis + ordinat * ordinat);
     }
 
     //mencari jarak
-    double getJarak (Titik T){
+    public double getJarak (Titik T){
         return Math.sqrt((absis - T.absis) * (absis - T.absis) + (ordinat - T.ordinat) * (ordinat - T.ordinat));
     }
 
     //mencari refleksi terhadap sumbu x
-    void refleksiX(){
+    public void refleksiX(){
         ordinat = -ordinat;
     }
 
     //mencari refleksi terhadap sumbu y
-    void refleksiY(){
+    public void refleksiY(){
         absis = -absis;
     }
 
     //mencari hasil titik setelah direfleksikan dengan sumbu x
-    Titik getRefleksiX(){
+    public Titik getRefleksiX(){
         return new Titik(absis, -ordinat);
     }
 
     //mencari hasil titik setelah direfleksikan dengan sumbu y
-    Titik getRefleksiY(){
+    public Titik getRefleksiY(){
         return new Titik(-absis, ordinat);
     }
 } //end class Titik
