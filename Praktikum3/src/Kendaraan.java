65e89c37cc9cf867be542eed15ec3e25a5b3d80e
Izzat Fauzan <mizzatfauzan11@gmail.com>
2026-03-03 14:03:14 +0700

    feat: add Kendaraan.java

---
+++ Praktikum3/src/Kendaraan.java
@@ -0,0 +1,38 @@
+public class Kendaraan {
+    
+    /* Atribut */
+    private String noPlat;
+    private String jenis;
+
+    /* Method */
+
+    //Konstruktor tanpa parameter
+    public Kendaraan(){
+        this.noPlat = "";
+        this.jenis = "";
+    }
+
+    //Konstruktor dengan parameter
+    public Kendaraan(String noPlat, String jenis){
+        this.noPlat = noPlat;
+        this.jenis = jenis;
+    }
+
+    //Selektor
+    public String noPlat(){
+        return noPlat;
+    }
+
+    public String jenis(){
+        return jenis;
+    }
+
+    //Mutator
+    public void setNoPlat(String noPlat){
+        this.noPlat = noPlat;
+    }
+
+    public void setJenis(String jenis){
+        this.jenis = jenis;
+    }
+}
